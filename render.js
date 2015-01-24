e96cdee6bc1c65b7c81811b6fd25e604da376caf
Copper Badger <thecopperbadger@gmail.com>
2015-01-24 14:58:25 -0500

    Rearranged setup scripts for Firefox add-on

--- render.js
+++ render.js
@@ -2,18 +2,6 @@ function capitalize(src){
 	return src?(src[0].toUpperCase()+src.substring(1)):src
 }
 
-// Set up globals
-window.fastyle = {}
-developerMode = window.fastyle.developerMode = document.location.hash == "#dev"
-disabled = window.fastyle.disabled = document.location.hash == "#nofastyle"
-isHttps = window.fastyle.isHttps = document.location.protocol == "https:"
-ss = "//maxcdn.bootstrapcdn.com/bootswatch/3.3.1/"+(sessionStorage.getItem('theme')||'simplex')+"/bootstrap.min.css"
-window.fastyle.funTitles=["Give us a Moment","Just a Second","Please Wait","Doing the Thing","Hold on a Sec","Stuff is Happening"]
-
-window.fastyle.truncateName = function(src) {
-	return (t=src.toLowerCase().match(/[a-z0-9\-\~\.]+/g))?t.join(''):"";
-}
-
 // Replace native style, make background color transition smooth
 precolor = $('body').css('background-color')
 	$('body').css({'background-color':precolor})
@@ -49,7 +37,7 @@ $(document).ready(function() {
 		.append("<div class='navbar-header'>")
 		.append("<div class='collapse navbar-collapse'>")
 
-	window.fastyle.uname = $('#my-username').text().substring(1)
+	
 
 	{
 		var consumebar = function(con,src,right){
