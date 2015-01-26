b059488bc15a2d397205475c3042273f867d7994
Copper Badger <thecopperbadger@gmail.com>
2015-01-26 17:22:06 -0500

    Added submission control page

---
+++ submissioncontrol.js
@@ -0,0 +1,79 @@
+function renderThumbs(){
+	idx = 0
+	var newRow = function(){return $("<div class='row well' style='margin:4px 0'>").appendTo('#submission-container')}
+	row = newRow()
+	$('[alt=thumbnail]').each(function(){
+
+		if(idx++==2){row = newRow();idx = 1}
+
+		par = $(this).parents('td[align=center]:first')
+
+		submissionAnchor = $(par).find('a[href*=view]')
+		submissionHref = $(submissionAnchor).attr('href')
+		submissionID = submissionHref.match(/\d+/)[0]
+		submissionThumb = submissionAnchor.find('img').attr('src')
+		deleteKey = $(par).find('.hlp').attr('onclick')
+			.match(/key=([^&]+)/)[1]
+		$(par).find('a,br').remove()
+		submissionName = $(par).text().replace(/\s*\[\s*\]\s*$/,'')
+
+		$('<div class="col-md-6">' +
+			'<div class="row submission-row">' +
+				'<div class="col-xs-4">' +
+					'<a href="'+submissionHref+'">' +
+						'<img src="'+submissionThumb+'" alt="thumbnail" class="img-responsive">' +
+					'</a>' +
+				'</div>' +
+				'<div class="col-xs-8">' +
+					'<h4 class="submission-title"><a href="'+submissionHref+'">'+submissionName+'</a></h4>' +
+					'<a href="javascript:void(0)" data-id="'+submissionID+'" data-key="'+deleteKey+'" class="btn btn-danger delete-confirm">Delete</a>' +
+				'</div>' +
+			'</div>' +
+		'</div>').appendTo(row)
+	})
+}
+
+skel = '<div id="submission-container"></div>' +
+'<div class="row">' +
+	'<div class="col-xs-12">' +
+			'<ul class="pagination">' +
+				'<li>' +
+					'<a href="" id="prev-page-button">Previous</a>' +
+				'</li>' +
+				'<li>' +
+					'<a href="" id="next-page-button">Next</a>' +
+				'</li>' +
+			'</ul>' +
+	'</div>' +
+'</div>'
+
+$(document).ready(function(){
+	$(skel).insertBefore('.content.maintable')
+
+	// Function is factored out in case we want to make load pages asynchronously
+	renderThumbs()
+
+	// Next / Previous paging buttons
+	pageNum = parseInt((t=document.location.pathname.match(/\d+/))?t[0]:0)
+	$('#next-page-button').attr('href','/controls/submissions/'+(pageNum+1)+'/')
+	if(!pageNum) {
+		$('#prev-page-button').parent('li').remove()
+	} else {
+		$('#prev-page-button').attr('href','/controls/submissions/'+(pageNum-1)+'/')
+	}
+
+	// Event Bindings
+	$('.delete-confirm').on("click",function(){
+		if(confirm("Are you absolutely sure you want to delete this submission? This cannot be undone.")){
+			tgt = $(this).parents('.submission-row:first').css({opacity:0.8})
+			$.ajax({
+				url: "/controls/submissions/delete/"+$(this).attr('data-id')+"/?key="+$(this).attr('data-key')+"&page="+pageNum,
+				type: "POST",
+				complete: function(){
+					$(this).parents('.submission-row:first').css({opacity:0.2})
+						.find('.submission-title').text("Deleted")
+				}
+			})
+		}
+	})
+})
