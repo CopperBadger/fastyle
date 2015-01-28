802ff015a32536520c9128f0d686ecf434757026
Copper Badger <thecopperbadger@gmail.com>
2015-01-28 17:32:08 -0500

    Rewrote gallery.js

--- gallery.js
+++ gallery.js
@@ -1,3 +1,5 @@
+skel = '<div id="gallery-container"></div>'
+
 function renderImages(src){
 	row = $('#gallery-container')
 	return $(src||'body').find('.r-general, .r-mature, .r-adult').each(function(){
@@ -25,30 +27,21 @@ function renderImages(src){
 	}).length
 }
 
-function serialize(el) {
-  var MD5 = function(src){return "";} //Dummy encryption function
-  var o={};
-  $.map($(el).find('input:not([type=radio]:not(:checked)), select, textarea'),function(e) {
-    if(n=(e=$(e)).prop('name')) {
-      o[n]=((e.is('[type=password]'))
-	        ?MD5(e.val())
-        :((e.is('[type=checkbox]'))
-          ?e.is(':checked')
-          :e.val()));
-    }
-  });
-  return o;
-}
-
 $(document).ready(function(){
 
-	if(document.location.pathname=="/msg/submissions/"){
-		row = $('<div class="row" id="gallery-controls">').insertBefore(".content.maintable:first")
-			.html('<button class="btn btn-default select-all">Select All</button> ' +
-				'<button class="btn btn-default deselect-all">Deselect All</button> ' +
-				'<button class="btn btn-primary submissions-clear">Remove Selected</button> ' +
-				'<button class="btn btn-danger nuke-submissions">Nuke All Submissions</button>')
-			.wrapInner('<div class="col=md-12">')
+	// First, insert and bind controls if this is the submissions page
+	if(document.location.pathname=="/msg/submissions/") {
+
+		row = $('<div class="row" id="gallery-controls">' +
+			'<div class="col-xs-12">' +
+				'<div class="btn-group">' +
+					'<button class="btn btn-default select-all" type="button">Select All</button>' +
+					'<button class="btn btn-default deselect-all" type="button">Deselect All</button>' +
+					'<button class="btn btn-primary submissions-clear" type="button">Remove Selected</button>' +
+					'<button class="btn btn-danger" type="button">Nuke All Submissions</button>' +
+				'</div>' +
+			'</div>' +
+		'</div>').insertBefore('.content.maintable')
 
 		$('.select-all, .deselect-all').on("click",function(){
 			$('.thumb-checkbox').find('[type=checkbox]').prop('checked',$(this).is('.deselect-all'))
@@ -71,96 +64,109 @@ $(document).ready(function(){
 
 		$('.nuke-submissions').on("click",function(){
 			$('#gallery-container').css({opacity:'0.8'})
-			$.ajax({
-				url: "/msg/submissions/",
-				type: "POST",
-				data: {'messagecenter-action':'Nuke all Submissions'},
-				complete:function(xhr){
-					$('#gallery-container').remove()
-				}
-			})
+			if(confirm("Are you sure you want to remove all submissions from your messages?")){
+				$.ajax({
+					url: "/msg/submissions/",
+					type: "POST",
+					data: {'messagecenter-action':'Nuke all Submissions'},
+					complete:function(xhr){
+						$('#gallery-container').remove()
+					}
+				})
+			}
 		})
 	}
 
-	row = $('<div class="row" id="gallery-container">').insertBefore(".content.maintable:first")
+
+	// Skeleton and data population
+	$(skel).insertBefore('.content.maintable')
 	renderImages()
 
-
-	page = (t=document.location.href.match(/\.net\/(.+)\d?\/?/))?t[1]:""
-	window.fastyle.pageNumber = parseInt((t=document.location.href.match(/(\d)\//))?t[1]:1)
-	window.fastyle.fetchSize = 36
-
-	if($('#no-images').length){
-		$('#gallery-container').append("<h3 style='text-align:center'>(No items to list)</h3>")
-	}
-
-	$('.content.maintable').hide()
-
-	window.fastyle.scrollThresh = Math.round($(document).height() - $(window).height()*2)
-	console.log("Initializing scroll thresh = "+window.fastyle.scrollThresh + ", doc height = "+$(document).height())
-
-	$('.content.maintable').show()
-
-	window.fastyle.clearToLoad = true
 	var imagesExist = $('.r-general, .r-mature, .r-adult').length
+	if($('#no-images').length || !imagesExist){
+		$('#gallery-container').append("<h3 style='text-align:center'>(No items to list)</h3>")
+	} else {
 
-	var loadMore = function(){
-		if(window.fastyle.clearToLoad) {
-			window.fastyle.clearToLoad = false
-			$('#load-more-button').text("Loading More...")
+		// Infinite Scroll
+		// Infinite scroll helper functions
+		var getScrollThresh = function(){return Math.round($(document).height()-$(window).height()*2)}
+		var getNextURL = function(){
+			out = ""
+			if(ta=(window.fastyle.nextHref||$('a.more:first').attr('href'))){
+				out = ta
+			} else {
+				out = document.location.protocol+"//"+window.fastyle.domain+".furaffinity.net/"+window.fastyle.pageString+"/"+window.fastyle.pageNumber
+			}
 
-			u = getNextURL()
-			d = serialize(sf=$('#search-form'));
-					d.perpage=window.fastyle.fetchSize
+			return out
+				.replace(/([^\:])\/{2,}/g,'$1/')
+				.replace(/search\/@.+/,'search/')
+		}
+
+		// Infinite Scroll variables
+		window.fastyle.pageNumber = ((t=document.location.pathname.match(/\/(d+)\/?$/g))?t:["","1"])
+		window.fastyle.pageString = document.location.pathname.replace(window.fastyle.pageNumber,window.fastyle.pageNumber[0])
+		window.fastyle.pageNumber = parseInt(window.fastyle.pageNumber[1])
+		window.fastyle.fetchSize = 36
+		window.fastyle.clearToLoad = true // If false, no loading will be allowed
+		window.fastyle.nextHref = null // The href attribute of a.more, if such an element exists
+
+		console.log("pageString = "+window.fastyle.pageString)
+
+		$('.content.maintable').hide()
+		window.fastyle.scrollThresh = getScrollThresh()
+		console.log("Threshold = "+window.fastyle.scrollThresh + ", doc height = "+$(document).height())
+		$('.content.maintable').show()
+
+		
+		var imagesExist = $('.r-general, .r-mature, .r-adult').length
+
+		var loadMore = function(){
+			if(window.fastyle.clearToLoad) {
+				window.fastyle.clearToLoad = false
+				$('#load-more-button').text("Loading More...")
+
+				window.fastyle.pageNumber++
+				u = getNextURL()
+
+				// For search results, get search params from form, incrementing page
+				d = window.fastyle.serialize(sf=$('#search-form'));
+					d.perpage = window.fastyle.fetchSize
 					d.go="Next"
 					if(sf.length){d.page = window.fastyle.pageNumber-1}
 					delete d.do_search
 
-			console.log("Triggered! Going to "+u)
-			$.ajax({
-				url:u,
-				type:(page=="browse/")?"GET":"POST",
-				data:d,
-				complete:function(xhr){
-					res = $(xhr.responseText)
-					window.fastyle.nextHref = $(res).find('a.more:first').attr('href')
-					n = renderImages(res)
-					console.log("Rendered "+n+" images")
+				console.log("Triggered! Going to "+u+" with parameters")
+				console.log(d)
 
-					if(n!=0) {
-						setTimeout(function(){
-							window.fastyle.clearToLoad = true
-							window.fastyle.scrollThresh = Math.round($(document).height() - $(window).height()*2)
-							$(document).trigger('scroll')
-							$('#load-more-button').text("Load More")
-						},5000)
-						console.log("Setting scroll thresh = "+window.fastyle.scrollThresh + ", doc height = "+$(document).height())
-					} else {
-						$('#load-more-button').before("<p>No more images to load!</p>").remove()
+				$.ajax({
+					url:u,
+					type:(window.fastyle.pageString=="/browse/")?"GET":"POST",
+					data:d,
+					complete:function(xhr){
+						res = $(xhr.responseText)
+						window.fastyle.nextHref = $(res).find('a.more:first').attr('href')
+						n = renderImages(res)
+						if(n!=0) {
+							setTimeout(function(){
+								window.fastyle.clearToLoad = true
+								window.fastyle.scrollThresh = getScrollThresh()
+								$(document).trigger('scroll')
+								$('#load-more-button').text("Load More")
+							},5000)
+						} else {
+							$('#load-more-button').before("<p>No more images to load!</p>").remove()
+						}
 					}
-				}
-			})
-		} else {
-			$('#load-more-button').text("Please wait...")
-			console.log("Failed to reinitiate: ctl="+window.fastyle.clearToLoad+", thresh="+window.fastyle.scrollThresh)
-		}
-	}
-
-	var getNextURL = function(){
-		out = ""
-		if(ta=(window.fastyle.nextHref||$('a.more:first').attr('href'))){
-			out = ta
-		} else {
-			out = document.location.protocol+"//"+window.fastyle.domain+".furaffinity.net/"+page+"/"+(++window.fastyle.pageNumber)
+				})
+			} else {
+				$('#load-more-button').text("Please wait...")
+				console.log("Failed to reinitiate: ctl="+window.fastyle.clearToLoad+", thresh="+window.fastyle.scrollThresh)
+			}
 		}
 
-		return out.replace(/([^\:])\/{2,}/g,'$1/').replace(/search\/@.+/,'search/')
-	}
-
-	if(page && imagesExist) {
-		$(document).scroll(function(){
+		$(document).scroll(function() {
 			if(window.fastyle.scrollThresh>0 && $(document).scrollTop() > window.fastyle.scrollThresh) {
-				
 				window.fastyle.scrollThresh = 0
 				loadMore()
 			}
@@ -176,6 +182,5 @@ $(document).ready(function(){
 			console.log("Setting scroll thresh = "+window.fastyle.scrollThresh + ", doc height = "+$(document).height())
 		})
 	}
-	$('#new-search-form').remove().insertBefore(row)
-	//$('.content.maintable').remove()
+
 })
