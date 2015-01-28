fe25acd566247094fdd7787912792b837322db5f
Copper Badger <thecopperbadger@gmail.com>
2015-01-28 18:03:25 -0500

    Tweak for footer

--- footer.js
+++ footer.js
@@ -1,14 +1,17 @@
-footskel = '<div class="row">' +
-	'<div class="col-xs-12">' +
-		'<div class="well">' +
-			'<div class="btn-group" id="footer-links"></div>' +
-			'<p id="footer-content" style="padding:16px 0"></p>' +
+footskel = '<div class="container">' +
+	'<div class="row">' +
+		'<div class="col-xs-12">' +
+			'<div class="well">' +
+				'<div class="btn-group" id="footer-links"></div>' +
+				'<p id="footer-content" style="padding:16px 0"></p>' +
+			'</div>' +
 		'</div>' +
 	'</div>' +
 '</div>'
 
+
 $(document).ready(function(){
-	$(footskel).appendTo('#body-wrapper')
+	$(footskel).insertBefore('.footer')
 
 	$('.footer a').addClass('btn btn-default')
 		.remove().appendTo('#footer-links')
